e8b92fe19102f40d578e3646acdb9e596119009f
KnQ4 <kcodewn@gmail.com>
2023-12-24 17:53:12 -0600

    updated contacts.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -37,9 +37,9 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: 'JavaScript Mastery',
+          to_name: 'Khanh Nguyen',
           from_email: form.email,
-          to_email: 'sujata@jsmastery.pro',
+          to_email: 'jcodewn@gmail.com',
           message: form.message,
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
@@ -87,7 +87,7 @@ const Contact = () => {
               name='name'
               value={form.name}
               onChange={handleChange}
-              placeholder="What can I call ya?"
+              placeholder="How can I address you?"
               className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
             />
           </label>
